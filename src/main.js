897444591fc5f08062193630722f943d29722a81
Luisa <lorenitaa889@gmail.com>
2022-12-28 13:04:23 -0500

    estructurando primeras interfaces

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { example } from './data.js';
+import ghibli from './data/ghibli/ghibli.json';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.json';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+// let url = ghibli ;
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error))

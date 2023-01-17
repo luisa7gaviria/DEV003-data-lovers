e68f55ebb4e398e097adc70527f440d6278412c3
Luisa <lorenitaa889@gmail.com>
2023-01-17 11:23:32 -0500

    diseño de modal y llamado interactivo

--- src/main.js
+++ src/main.js
@@ -1,13 +1,45 @@
 import data from './data/ghibli/ghibli.js'; //importar datos
-import { drawFilms , orderAlphabet } from './data.js';
+import { drawFilms , orderingBy} from './data.js';
 
-console.log(orderAlphabet ,data); 
+const getData = data.films
 
+//Mostrando datos en la interfaz
 const dataContainerEl = document.querySelector("#data-container")
+dataContainerEl.innerHTML = drawFilms(getData)
 
-dataContainerEl.innerHTML = drawFilms(data.films)
 
+//ordenando por medio de un select
+const drawRating = document.getElementById('desplegar-orden')
+const release = document.getElementById('ordenar-fecha').value
+const score = document.getElementById('ordenar-rt').value
 
+drawRating.addEventListener("change" , () =>{ 
+  if(drawRating.value === score){dataContainerEl.innerHTML = drawFilms(orderingBy(getData , 'rt_score'))}
+  if(drawRating.value === release){dataContainerEl.innerHTML = drawFilms(orderingBy(getData , 'release_date'))}
+  if(drawRating.value === ""){dataContainerEl.innerHTML = drawFilms(getData)}
   
+})
+
+ // Mostrando/Quitando modal
+const modal = document.querySelector('.modal')
+const showModal = document.getElementsByClassName('film-container') 
+const closeModal = document.querySelector('#close-modal')
+
+for( let i=0 ; i< showModal.length ; i++){
+  showModal[i].addEventListener("click" , () =>{
+  modal.style.display = "block";
+  
+})
+}
+closeModal.addEventListener("click" , () =>{
+  modal.style.display = "none";
+})
+
+
+
+console.log(document.querySelectorAll('.film-container'))
+console.log(document.querySelector('.film-container'))
+
+
      
  

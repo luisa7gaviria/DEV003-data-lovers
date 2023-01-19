49ee7f9a2d24d79b3f32f77c7c310f18fc418e8a
Luisa <lorenitaa889@gmail.com>
2023-01-19 11:58:46 -0500

    cambios en CSS y data.js

--- src/data.js
+++ src/data.js
@@ -1,29 +1,31 @@
-export function drawFilms(filmsArray){               //Funcion cuyo unico fin es PINTAR, DEVOLVER EL HTML DE CADA FILM
+
+export const orderingBy = (data , key) =>{  
+
+  return [... data].sort((a, b) => {                      
+   return  b[key] - a[key]
+  })  
+
+} 
+
+// export const searchBy = (movie , search) =>{
+//   if (movie === search){
+//     movie.filter((item => item === search)
+//      // no se como seria para un personaje
+//   )
+//   }else{
+//     throw new Error('Tu búsqueda no coincide')
+//   }
   
-  return filmsArray.map( function(film){ 
+  
+// }
+//export const filtering = (toFilter) => toFilter.filter(filtrao => filtrao.propiedad === 'valor de esa key') //especie genero : personajes
+  
 
-    return `
-    <div class="film-container">
-      <h4>${film.title} </h4> 
-      <p> ${film.release_date} </p>
-    </div>
-    `
-  } ).join('')
-};
 
-export const orderAlphabet = (film) =>{
-  film.sort((a, b) => {
-  a = a.toLowerCase();
-  b = b.toLowerCase();
-  if (a == b) {
-    return 0;
-  }
-  if (a < b) {
-    return -1;
-  }
-  return 1;
-});
-}
+
+
+
+
 
 
 

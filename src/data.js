33a09223b7028f9dedb8447bd39b95084345a823
Luisa <lorenitaa889@gmail.com>
2023-01-19 12:00:52 -0500

    ordenando

--- src/data.js
+++ src/data.js
@@ -6,19 +6,8 @@ export const orderingBy = (data , key) =>{
   })  
 
 } 
+//funcion ordenar
 
-// export const searchBy = (movie , search) =>{
-//   if (movie === search){
-//     movie.filter((item => item === search)
-//      // no se como seria para un personaje
-//   )
-//   }else{
-//     throw new Error('Tu búsqueda no coincide')
-//   }
-  
-  
-// }
-//export const filtering = (toFilter) => toFilter.filter(filtrao => filtrao.propiedad === 'valor de esa key') //especie genero : personajes
   
 
 

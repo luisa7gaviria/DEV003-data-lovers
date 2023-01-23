cad3d0d97a88b103e8618b0b2a70611af8f5408c
Luisa <lorenitaa889@gmail.com>
2023-01-23 16:45:31 -0500

    dom y función buscar

--- src/data.js
+++ src/data.js
@@ -1,11 +1,46 @@
 
-export const orderingBy = (data , key) =>{  
+export function orderingBy (data , key){  
 
   return [... data].sort((a, b) => {                      
    return  b[key] - a[key]
   })  
 
+} 
+
+export const filteringD = (data , keyvalue) => {
+  const filtered = data.filter((movie) => {
+    if(movie.director === keyvalue ){
+      return true
+    } 
+    if( keyvalue === 'Others' ){ 
+      return movie.director !== 'Hayao Miyazaki' && 'Isao Takahata' 
+     }
+  })
+ return filtered
 }
+
+export const filteringP = (data , keyvalue) => {
+  const filteredP = data.filter((movie) => {
+    if(movie.producer === keyvalue ){
+      return true
+    } 
+    if( keyvalue === 'Others' ){ 
+      return movie.producer !== 'Toshio Suzuki' 
+     }
+  })
+ return filteredP
+}
+
+export const searchInput = ( text , data ) => {     
+  const match = data.filter(item =>{
+    return  item.title.toLowerCase().includes( text )
+})
+return match
+}
+
+
+
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,86 +1,74 @@
 import data from './data/ghibli/ghibli.js'; //importar datos
-import { orderingBy } from './data.js';
+import { orderingBy, filteringD, filteringP , searchInput } from './data.js';
 
-//variable donde almacenamos los datos para trabajar más comodamente
-const getData = data.films
+const ghibliData = data.films;
 
 // funcion para pintar datos 
-function drawFilms(filmsArray){               //Funcion cuyo unico fin es PINTAR, DEVOLVER EL HTML DE CADA FILM
-  
-  let containerPicture = document.getElementById('data-container');
- const picture = filmsArray.map( function(film){ 
-
-     return  `
-    <div class="film-container" id= " ${film.id} ">
+function drawFilms (filmsArray) {               
+    let movies = '';
+    filmsArray.forEach((film) =>{ 
+ 
+     movies +=  `
+    <div class="film-container" >
       
-      <h4 class="title">${film.title} </h4> 
+      <h3 class="title">${film.title} </h3> 
       <img src="${film.poster}">
       <h4> 📆 ${film.release_date} </h4>
       <h5> ⭐ ${film.rt_score} </h5>
-      
-    </div>
-           
-    ` 
+      <button id="${film.id}"class="see-more" > More</button> 
 
-  } ).join('')
+    </div> `;
+         
+  })
+ document.getElementById('data-container').innerHTML = movies;
+};
+drawFilms(ghibliData);
 
-  containerPicture.innerHTML = picture
-  
-  console.log(containerPicture.querySelectorAll('.film-container'))
-  return containerPicture
-  
+//mostrando los datos ordenados
+document.getElementById('desplegar-orden').addEventListener("change" , () =>{
+    const selectOrden = document.getElementById('desplegar-orden').value;
+    const selectScr = document.getElementById('ordenar-rt').value;
+    const selectRel = document.getElementById('ordenar-fecha').value;
 
-
-}
-document.querySelectorAll('.film-container').forEach((items) =>{
-  items.addEventListener("click" , contModal )
-}) 
-// funcion para pintar datos del modal 
-
-const contModal = (ghibli) =>{
-  return `
-  <div class="modal">
-   
-  <div class="modal-header"> Datos extrasss
-    <span id="close-modal"> &times;</span>
-  </div>
-    
-   <div id="modal-info">
- 
-    <div class="modal-content">
- 
-      <h4> Director: ${ghibli.director} </h4> 
-      <h4> Productor: ${ghibli.producer} </h4>
-      <h4> Rating: ${ghibli.rt_score} </h4>
-      <h4> Description: ${ghibli.description} </h4>
-
-    </div>
-  </div>
- 
-</div>
-  `
-}
-
-//Mostrando datos en la interfaz
- drawFilms(getData) 
-
-
-
-//ordenando por medio de un select
-const drawRating = document.getElementById('desplegar-orden')
-const release = document.getElementById('ordenar-fecha').value
-const score = document.getElementById('ordenar-rt').value
-const dataContainerEl = document.getElementById('data-container')
-
-// ejecutamos funcion de pintar con los nuevos arreglos
-
-drawRating.addEventListener("change" , () =>{ 
-  if(drawRating.value === score){dataContainerEl.innerHTML = drawFilms(orderingBy(getData , 'rt_score'))  }
-  if(drawRating.value === release){dataContainerEl.innerHTML = drawFilms(orderingBy(getData , 'release_date')) }
-  if(drawRating.value === ""){dataContainerEl.innerHTML = drawFilms(getData)}
-  
+  if(selectOrden === selectScr){ drawFilms(orderingBy(ghibliData , 'rt_score')) };
+  if(selectOrden === selectRel){ drawFilms(orderingBy(ghibliData , 'release_date')) };
 })
 
 
+//evento y  funcion que pinta los directores filtrados
+
+  document.getElementById('director-h').addEventListener("click" , ()=>{
+    drawFilms(filteringD(ghibliData , 'Hayao Miyazaki'));
+    document.getElementById('select-orden-films').style.opacity = 0;
+  });
+  document.getElementById('director-i').addEventListener("click" , ()=>{
+    drawFilms(filteringD(ghibliData , 'Isao Takahata'));
+    document.getElementById('select-orden-films').style.opacity = 0;
+  });
+  document.getElementById('directors').addEventListener("click" , ()=>{
+    drawFilms(filteringD(ghibliData , 'Others'));
+    document.getElementById('select-orden-films').style.opacity = 0;
+  });
+  
+ 
+
+// evento y funcion que pinta los productores filtrados
+
+  document.getElementById('producer-t').addEventListener("click" , ()=>{
+    drawFilms(filteringP(ghibliData , 'Toshio Suzuki'))
+    document.getElementById('select-orden-films').style.opacity = 0;
+  });
+  document.getElementById('producers').addEventListener("click" , ()=>{
+    document.getElementById('select-orden-films').style.opacity = 0;
+    drawFilms(filteringP(ghibliData , 'Others'));
+  });
 
 
+ //  funcion de buscar
+
+document.getElementById('search-input').addEventListener("keyup" , (e) =>{
+  let input = e.target.value
+  const result = searchInput(input , ghibliData)
+  drawFilms(result)
+  
+})
